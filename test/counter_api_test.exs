15cfcc6ab3fc9b4cb556771e08f37e85da6cfff9
Churchill Aboge <tchillo@gmail.com>
2019-09-28 20:01:20 +0300

    Add test cases

---
+++ test/counter_api_test.exs
@@ -0,0 +1,12 @@
+defmodule CounterApiTest do
+  use ExUnit.Case
+
+  test "use counter through API do" do
+    pid = Counter.start(0)
+    assert Counter.state(pid) == 0
+
+    Counter.tick(pid)
+    Counter.tick(pid)
+    assert Counter.state(pid) == 2
+  end
+end
